53efe66b6ec7b3e48e5699d72af5bec934ab4cd0
kaushalshivang-ai <shivang.25bai10965@vitbhopal.ac.in>
2026-08-10 03:10:17 +0530

    r

--- RectangleProgram.java
+++ RectangleProgram.java
@@ -2,20 +2,18 @@ import java.util.Scanner;
 
 public class RectangleProgram {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        try (Scanner input = new Scanner(System.in)) {
+            System.out.print("Enter length: ");
+            double l = input.nextDouble();
 
-        System.out.print("Enter length: ");
-        double l = input.nextDouble();
+            System.out.print("Enter breadth: ");
+            double b = input.nextDouble();
 
-        System.out.print("Enter breadth: ");
-        double b = input.nextDouble();
+            double area = l * b;
+            double perimeter = 2 * (l + b);
 
-        double area = l * b;
-        double perimeter = 2 * (l + b);
-
-        System.out.printf("Area of Rectangle: %.2f\n", area);
-        System.out.printf("Perimeter of Rectangle: %.2f\n", perimeter);
-
-        input.close();
+            System.out.printf("Area of Rectangle: %.2f\n", area);
+            System.out.printf("Perimeter of Rectangle: %.2f\n", perimeter);
+        }
     }
 }
